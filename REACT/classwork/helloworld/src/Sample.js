025bc42d2c752370763cf74da801141f94823a75
AnitaPannerselvam <125266023+AnitaPannerselvam@users.noreply.github.com>
2023-12-09 14:32:21 +0530

    I have created a reactjs in nodejs

---
+++ REACT/classwork/helloworld/src/Sample.js
@@ -0,0 +1,23 @@
+function Sample()
+{
+    return(
+        <div>
+          <p>This is a Sample Component</p>
+          </div>
+    );
+}
+export default Sample
+/*function sum(...args){
+    let sum=0;
+    for(let i=0;i<args.length;i++){
+    sum+=args[i];
+}
+return sum;
+}
+console.log(sum(2,4,5,6))
+console.log(sum(20,30,40))*/
+
+
+
+
+
